4ed77399bb9029e62f66e3a145f280e40df3b83e
jacobwright <jacob.thewrightone@gmail.com>
2024-04-02 18:07:05 -0500

    move hello world to new component

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,12 +1,14 @@
-import Image from "next/image";
-
+function HelloWorld() {
+  return (
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      Hello World!
+    </div>
+  );
+}
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Hello World!
-      </div>
-
+      <HelloWorld />
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
